013d0e03e358821601b76cb6329e6ea39c3f55fb
かおり <m.kaori5.12@disney.ne.jp>
2024-04-16 07:29:46 +0900

    エラーの出るファイルの追加

---
+++ src/resources/views/find.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+<style>
+    th {
+        background-color: #289ADC;
+        color: white;
+        padding: 5px 40px;
+    }
+    td {
+        padding: 25px 40px;
+        background-color: #EEEEEE;
+        text-align: center;
+    }
+</style>
